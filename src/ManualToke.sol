85526d030b29cf5f333affc00c3fa50509dea6d7
veslorand <veslorand@gmail.com>
2024-04-07 23:04:49 +0200

    Add OpenZeppelin contracts submodule and remapping for OpenZeppelin in foundry.toml

---
+++ src/ManualToke.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract ManualToken {
+
+mapping (address => uint) private s_balances;
+
+    function name() public pure returns (string memory) {
+        return "Manual Token";
+    }
+
+    function totalSupply() public pure returns (uint256)
+    {
+        return 100 ether;
+    }
+
+    function decimals() public pure returns (uint8)
+    {
+        return 18;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256)
+    {
+        return s_balances[_owner];
+    }
+
+    function transfer(address _to, uint256 _amount) public returns (bool success)
+    {
+        uint256 previuosBalances = balanceOf(msg.sender) + balanceOf(_to);
+        s_balances[msg.sender] -= _amount;
+        s_balances[_to] += _amount;
+        require(balanceOf(msg.sender) + balanceOf(_to) == previuosBalances);
+        return true;
+    }
+}
