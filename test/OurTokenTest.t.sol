61381baa0f7dd89f475051604aa57bd09b0a0018
veslorand <veslorand@gmail.com>
2024-04-08 20:19:33 +0200

    Fix test functions in OurTokenTest.t.sol

--- test/OurTokenTest.t.sol
+++ test/OurTokenTest.t.sol
@@ -6,6 +6,8 @@ import {DeployOurToken} from "../script/DeployOurToken.s.sol";
 import {Test} from "forge-std/Test.sol";
 import {OurToken} from "../src/OurToken.sol";
 
+error TransferToZeroAddressNotAllowed();
+
 contract OurTokenTest is Test {
     OurToken public ourToken;
     DeployOurToken public deployer;
@@ -23,7 +25,7 @@ contract OurTokenTest is Test {
         ourToken.transfer(bob, STARTING_BALANCE);
     }
 
-    function testBobBalance() public {
+    function testBobBalance() public view {
         assertEq(ourToken.balanceOf(bob), STARTING_BALANCE);
     }
 
@@ -40,5 +42,42 @@ contract OurTokenTest is Test {
 
         assertEq(ourToken.balanceOf(alice), transferAmount);
         assertEq(ourToken.balanceOf(bob), STARTING_BALANCE - transferAmount);
+    }
+
+    function testTransferToSelf() public {
+        uint256 bobBalanceBefore = ourToken.balanceOf(bob);
+        vm.prank(bob);
+        ourToken.transfer(bob, STARTING_BALANCE / 2);
+        uint256 bobBalanceAfter = ourToken.balanceOf(bob);
+
+        // Balance should remain the same after transferring to self
+        assertEq(
+            bobBalanceBefore,
+            bobBalanceAfter,
+            "Bob's balance should remain unchanged after transferring to self"
+        );
+    }
+
+    function testAllowanceResetAfterTransferFrom() public {
+        uint256 initialAllowance = 500;
+        vm.prank(bob);
+        ourToken.approve(alice, initialAllowance);
+
+        uint256 transferAmount = 200;
+        vm.prank(alice);
+        ourToken.transferFrom(bob, alice, transferAmount);
+
+        assertEq(
+            ourToken.allowance(bob, alice),
+            initialAllowance - transferAmount,
+            "Allowance should be decreased by the transferred amount"
+        );
+    }
+
+    function testCannotTransferFromWithoutAllowance() public {
+        uint256 transferAmount = 100;
+        vm.prank(alice);
+        vm.expectRevert(); // Expect any revert
+        ourToken.transferFrom(bob, alice, transferAmount);
     }
 }
